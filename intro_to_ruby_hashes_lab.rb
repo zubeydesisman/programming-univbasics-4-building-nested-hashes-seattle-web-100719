a29317bddf8efbf6a1fb692f60295292167f130b
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 20:28:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_fourth_tier
     :names => {:reading_railroad =>{"mortgage_value" => "$100"},
     :pennsylvania_railroad => {"mortgage_value" => "$200"},
     :b_and_o_railroad => {"mortgage_value" => "$400"},
-    :shortline_railroad => {}
+    :shortline_railroad => {"mortgage_value" => "$800"}
     },
    }
   }
