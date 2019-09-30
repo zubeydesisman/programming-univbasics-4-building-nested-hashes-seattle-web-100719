882dc869fd83ef23f9a642d9663f94ead3c3011f
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 20:00:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {:rent_in_dollars => {key1:"",key2:"",key3:"",key4:""},names:""}}
+  {railroads: {:rent_in_dollars => {key1:"",key2:"",key3:"",key4:""},names: {keyone:"",keytwo:"",keythree:"",keyfour:""}}}
 
 end
 
