7aa899556b29d178d671f2c8a8b377249ed112fb
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 20:15:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_third_tier
   {railroads: {
     :rent_in_dollars => {:one_piece_owned => 25,:two_pieces_owned => 50, :three_pieces_owned => 100,:four_pieces_owned => 200},
-    :names => {keyone:"",keytwo:"",keythree:"",keyfour:""},
+    :names => {:reading_railroad =>{},keytwo:"",keythree:"",keyfour:""},
     
   }
   }
